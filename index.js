5a1488a9a54d30b923086b8af57e68e849d440b0
Stand114514 <1687771866@qq.com>
2023-12-27 16:37:21 +0800

    优化背景图

--- index.js
+++
@@ -1,5 +0,0 @@
-// evnet_html = `<div class="enent">\
-//                 <p class="title">${title}</p>\
-//                 <p class="content">${content}</p>\
-//                 <p class="change-date">${time}</p>\
-//             </div>`
